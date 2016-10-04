d0113df4c4cc9ed22b85da9a4404759277c93c75
Andrew Jensen <andrewjensen90@gmail.com>
2016-10-04 00:10:52 -0600

    Connect CLI tool to Analyzer

--- packages/hekla-cli/src/commands/analyze.js
+++ packages/hekla-cli/src/commands/analyze.js
@@ -1,3 +1,22 @@
+'use strict';
+
+const path = require('path');
+const Analyzer = require('hekla-core').Analyzer;
+
 module.exports = function analyze() {
-  console.log('analyzing...');
+  let config;
+
+  try {
+    config = require(getHeklafilePath());
+  } catch (err) {
+    console.error('Configuration error:', err.stack);
+    process.exit(1);
+  }
+
+  const analyzer = new Analyzer(config);
+  analyzer.run();
 };
+
+function getHeklafilePath() {
+  return path.resolve(process.cwd(), 'Heklafile.js');
+}
