bbcb3f0e739e43e3d98162348441dfe6a077dd17
Andrew Jensen <andrewjensen90@gmail.com>
2016-12-05 21:32:29 -0700

    Implement adding nodes to the graph

--- packages/hekla-viewer/src/ContentPane/DependencyChart/index.js
+++ packages/hekla-viewer/src/ContentPane/DependencyChart/index.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import ComponentContextMenu from './ComponentContextMenu';
 import ComponentBox from './ComponentBox';
 import ComponentDependencyArrow from './ComponentDependencyArrow';
+const DependencyGraph = require('hekla-core').DependencyGraph;
 
 import './DependencyChart.css';
+
+const OFFSET_X = 48;
+const OFFSET_Y = 100;
+const GRID_X = 250;
+const GRID_Y = 100;
 
 export default class DependencyChart extends Component {
   constructor(props) {
@@ -11,11 +17,19 @@ export default class DependencyChart extends Component {
     this._onSelect = this._onSelect.bind(this);
     this._onContextMenu = this._onContextMenu.bind(this);
     this._onClickBackground = this._onClickBackground.bind(this);
+    this._onUpdateSelectedComponent = this._onUpdateSelectedComponent.bind(this);
+    this._nextY = 0; // TODO: make this smarter
     this.state = {
-      subgraph: {},
+      subgraph: new DependencyGraph(),
       contextMenuComponent: null,
       contextMenuCoordinates: { x: 0, y: 0 }
     };
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.selectedComponent !== this.props.selectedComponent) {
+      this._onUpdateSelectedComponent(newProps.selectedComponent);
+    }
   }
 
   /**
@@ -43,6 +57,26 @@ export default class DependencyChart extends Component {
     });
   }
 
+  _onUpdateSelectedComponent(component) {
+    const { subgraph } = this.state;
+    if (!subgraph.hasNode(component.id)) {
+      // Add the node
+      // Pick coordinates for the box
+      const boxX = 0;
+      const boxY = this._nextY;
+      this._nextY++;
+      const node = makeNode(component, boxX, boxY);
+      subgraph.addNode(component.id, node);
+
+      // Add links to and from other nodes in the current subgraph
+      // TODO: implement
+
+      // Re-render
+      console.log('new subgraph:', subgraph);
+      this.forceUpdate();
+    }
+  }
+
   _onClickBackground() {
     // Close the context menu
     this.setState({
@@ -53,6 +87,8 @@ export default class DependencyChart extends Component {
   render() {
     const { components, selectedComponent } = this.props;
     const { contextMenuComponent, contextMenuCoordinates } = this.state;
+    const subgraphNodes = this.state.subgraph.nodes.map(node => node.value);
+    const subgraphLinks = this.state.subgraph.links;
     return (
       <div ref="container" className="DependencyChart" onClick={this._onClickBackground}>
         {!contextMenuComponent ? null : (
@@ -73,40 +109,45 @@ export default class DependencyChart extends Component {
             orient="auto">
             <path d="M 0 0 L 10 5 L 0 10 z" />
           </marker>
-          <ComponentBox
-            x={48}
-            y={100}
-            component={components[0]}
-            selected={selectedComponent === components[0]}
-            onSelect={this._onSelect}
-            onContextMenu={this._onContextMenu}
-          />
-          <ComponentBox
-            x={48}
-            y={200}
-            component={components[1]}
-            selected={selectedComponent === components[1]}
-            onSelect={this._onSelect}
-            onContextMenu={this._onContextMenu}
-          />
-          <ComponentBox
-            x={298}
-            y={200}
-            component={components[3]}
-            selected={selectedComponent === components[3]}
-            onSelect={this._onSelect}
-            onContextMenu={this._onContextMenu}
-          />
-          <ComponentDependencyArrow
-            from={{ x: 148, y: 150 }}
-            to={{ x: 148, y: 200 }}
-          />
-          <ComponentDependencyArrow
-            from={{ x: 148, y: 150 }}
-            to={{ x: 398, y: 200 }}
-          />
+          {subgraphNodes.map(node => (
+            <ComponentBox
+              key={node.component.id}
+              x={node.x}
+              y={node.y}
+              component={node.component}
+              selected={selectedComponent === node.component}
+              onSelect={this._onSelect}
+              onContextMenu={this._onContextMenu}
+            />
+          ))}
+          {subgraphLinks.map(link => {
+            const { fromNode, toNode } = link;
+            const key = `${fromNode.component.id}-${toNode.component.id}`;
+            return (
+              <ComponentDependencyArrow
+                key={key}
+                from={{ x: fromNode.x, y: fromNode.y }}
+                to={{ x: toNode.x, y: toNode.y }}
+              />
+            );
+          })}
         </svg>
       </div>
     );
   }
 };
+
+function makeNode(component, gridX, gridY) {
+  return {
+    x: (OFFSET_X + (gridX * GRID_X)),
+    y: (OFFSET_Y + (gridY * GRID_Y)),
+    component
+  };
+}
+
+function makeLink(fromNode, toNode) {
+  return {
+    fromNode,
+    toNode
+  };
+}
