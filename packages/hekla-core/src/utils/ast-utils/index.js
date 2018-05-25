4fc5e374b4253ecec909acd476a6a0382cc38e1f
Andrew Jensen <andrewjensen90@gmail.com>
2018-05-24 19:02:35 -0600

    Add looksLike function for comparing AST nodes

--- packages/hekla-core/src/utils/ast-utils/index.js
+++ packages/hekla-core/src/utils/ast-utils/index.js
@@ -10,6 +10,7 @@ module.exports = {
   filterNodes,
   isPromiseCall,
   isASTNode,
+  looksLike,
   getDeepProperty,
   reduceCallName,
   reduceMemberName,
@@ -47,6 +48,25 @@ function isPromiseCall(callExpNode) {
 
 function isASTNode(value, key, parent) {
   return (value && typeof value === 'object' && value.type);
+}
+
+function looksLike(a, b) {
+  return (
+    a &&
+    b &&
+    Object.keys(b).every(bKey => {
+      const bVal = b[bKey]
+      const aVal = a[bKey]
+      if (typeof bVal === 'function') {
+        return bVal(aVal)
+      }
+      return isPrimitive(bVal) ? bVal === aVal : looksLike(aVal, bVal)
+    })
+  )
+}
+
+function isPrimitive(val) {
+  return val == null || /^[sbn]/.test(typeof val)
 }
 
 /**
