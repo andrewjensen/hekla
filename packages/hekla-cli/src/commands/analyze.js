c39fa5d2973c588681c8df1e69ab891c73e98314
Andrew Jensen <andrewjensen90@gmail.com>
2018-09-19 21:20:12 -0600

    Stub out `--single` option

--- packages/hekla-cli/src/commands/analyze.js
+++ packages/hekla-cli/src/commands/analyze.js
@@ -1,23 +1,32 @@
-'use strict';
-
 const path = require('path');
-const Analyzer = require('hekla-core').Analyzer;
 
-module.exports = function analyze() {
-  let config;
-  const configPath = getHeklafilePath();
+module.exports = function analyze(cmd) {
+  const { single } = cmd;
 
-  try {
-    config = require(configPath);
-  } catch (err) {
-    console.error('Configuration error:', err.stack);
-    process.exit(1);
+  console.log('Analyzing!');
+
+  if (single) {
+    console.log('Analyze just a single file:', single);
+
+    const filePath = path.resolve(process.cwd(), single);
+
+    console.log('Resolved:', filePath);
   }
 
-  const analyzer = new Analyzer(config, configPath);
-  analyzer.run();
+  // let config;
+  // const configPath = getHeklafilePath();
+
+  // try {
+  //   config = require(configPath);
+  // } catch (err) {
+  //   console.error('Configuration error:', err.stack);
+  //   process.exit(1);
+  // }
+
+  // const analyzer = new Analyzer(config, configPath);
+  // analyzer.run();
 };
 
-function getHeklafilePath() {
-  return path.resolve(process.cwd(), 'Heklafile.js');
-}
+// function getHeklafilePath() {
+//   return path.resolve(process.cwd(), 'Heklafile.js');
+// }
