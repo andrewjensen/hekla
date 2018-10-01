1ef7f80c27c75e538babff3495e64cacb3a23f0f
Andrew Jensen <andrewjensen90@gmail.com>
2018-10-01 12:33:28 -0600

    Implement ASTWrapper#visit, with the help of babel type definitions

--- packages/hekla-core/src/utils/ast-utils/ASTWrapper.js
+++ packages/hekla-core/src/utils/ast-utils/ASTWrapper.js
@@ -1,4 +1,8 @@
 const walk = require('tree-walk');
+const { VISITOR_KEYS } = require('@babel/types');
+
+// TODO: define a custom walker based on VISITOR_KEYS
+// (See DOMWalker)
 
 module.exports = class ASTWrapper {
   constructor(ast) {
@@ -7,5 +11,24 @@ module.exports = class ASTWrapper {
 
   unwrap() {
     return this.ast;
+  }
+
+  visit(visitors) {
+    for (let key in visitors) {
+      if (!VISITOR_KEYS.hasOwnProperty(key)) {
+        throw new TypeError(`Invalid visitor type: ${key}`);
+      }
+    }
+
+    walk.preorder(this.ast, (node) => callVisitor(node, visitors, this.ast));
+  }
+}
+
+function callVisitor(node, visitors, dom) {
+  if (!node) {
+    return;
+  }
+  if (node.type && visitors[node.type]) {
+    visitors[node.type](node);
   }
 }
